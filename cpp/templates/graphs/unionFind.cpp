c08329a3454554dab5f2a626bc4296dfeb0d162a
rayvivek881 <rayvivek881@gmail.com>
2025-05-13 03:27:09 +0530

    done

--- cpp/templates/graphs/unionFind.cpp
+++ cpp/templates/graphs/unionFind.cpp
@@ -1,27 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 template <typename T>
-class UnionFind
-{
+class UnionFind {
 public:
   int Components;
   vector<T> parent, rank;
-  UnionFind(int size) : Components(size)
-  {
+  UnionFind(int size) : Components(size) {
     parent.resize(size);
     rank.assign(size, 0);
     for (int i = 0; i < size; i++)
       parent[i] = i;
   }
-  T find(T x)
-  {
+  T find(T x) {
     if (parent[x] != x)
       parent[x] = find(parent[x]);
     return parent[x];
   }
   int size() { return Components; }
-  void Union(T a, T b)
-  {
+  void Union(T a, T b) {
     T aset = find(a), bset = find(b);
     if (aset == bset)
       return;
