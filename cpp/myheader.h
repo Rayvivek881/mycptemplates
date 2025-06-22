3bd4abc3a1a30c64f4b61ecc29cbf254d9497151
rayvivek881 <rayvivek881@gmail.com>
2025-06-22 20:52:29 +0530

    header added

---
+++ cpp/myheader.h
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<cstdint>
+#include<vector>
+#include<string>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<queue>
+
+using namespace std;
